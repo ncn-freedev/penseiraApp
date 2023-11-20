d9c30aa0d204e6e5eca3c81cb67321aeeb87f7eb
Nicolas <ncn.freedev@gmail.com>
2023-11-19 21:36:20 -0300

    feat: Creating project in vscode, remembering flutter

---
+++ penseira_app/android/app/src/main/kotlin/com/example/penseira_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.penseira_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ penseira_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:penseira_app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
